2b4b42a670798242fd56f315f39a1bcbdfd93cb0
Annisa <annisakaraca@gmail.com>
2018-02-13 11:39:50 -0800

    Implement basic post request and parse request body

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -2,12 +2,17 @@
 
 // view
 $(document).ready(function(){
-    console.log('window ready');
-    $('#submit-btn').on("click", clickHandler);
+  console.log('window ready');
+  $('#submit-btn').on("click", clickHandler);
 })
 
 // controller
 
 var clickHandler = function(){
-    console.log('clicked submit!');
+  console.log('clicked submit!');
+  // make post request
+  $.post("/", {text: 'test post request', hello: 'world'}, function(data, status) {
+    console.log('data', data);
+  });
 }
+
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,8 +3,8 @@ const app = express();
 
 // middleware
 var myLogger = function (req, res, next) {
-    console.log(req.method + 'request receieved');
-    next();
+  console.log(req.method + ' request receieved');
+  next();
   } ;
 
 app.use(express.static('client'));
@@ -12,6 +12,27 @@ app.use(myLogger);
 
 
 app.get('/', (req, res) => res.send('hello'));
+app.post('/', (req,res) => {
+  let body = [];
+  req.on('data', (chunk) => {
+    console.log('collecting data');
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString('utf8');
+    var bodyAttributes = body.split('&');
+    var bodyObj = {};
+    bodyAttributes.forEach(function(attr) {
+      var tuple = attr.split('=');
+      var key = tuple[0];
+      var value = tuple[1];
+      var words = value.split('+');
+      value = words.join(' ');
+      bodyObj[key] = value;
+    })
+    console.log('body', bodyObj);
+  })
+  res.send('got post')
+});
 
 app.listen(3000, () => console.log('CSV report app listening on port 3000!'));
 
