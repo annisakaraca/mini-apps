50ddd5ff5c9e2e602d57d7e0da771117048e0f40
Annisa <annisakaraca@gmail.com>
2018-02-12 17:26:44 -0800

    Refactor so that all code related to user input is in controller object

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -20,95 +20,93 @@ var model = {
 
 // CONTROLLER
 
-var playMove = function(x,y) {
-  renderMsg('');
-  // check if move is valid
-  if (model.board[x][y] !== 0) {
-    renderMsg('invalid move');
-  } else {
-    // update board array
-    model.board[x][y] = model.currentPlayer;
-    updateBoardView(x,y);
-    model.moveCounter++;
-    checkGameProgress();
-    toggleCurrentPlayer();
-  }
-};
-
-var toggleCurrentPlayer = function() {
-  if (model.currentPlayer === 'X') {
-      model.currentPlayer = 'O';
-  } else {
-      model.currentPlayer = 'X';
-  }
-  renderCurrentPlayer();
-};
-
-var checkGameProgress = function() {
-  var hasWinner = checkIfWinner();
-//  var hasWinner = false;
-  if (hasWinner) {
-    // render message to screen
-    renderMsg('WINNER: ' + model.playerName[model.currentPlayer]);
-    model.lastWinner = model.currentPlayer;
-    model.score[model.currentPlayer]++;
-    //update score board
-    updateScoreBoard(model.currentPlayer);
-  } else if (model.moveCounter === 9) {
-    // render tie message to screen
-    renderMsg('Game over: TIE');
-  }
-};
-
-var checkIfWinner = function() {
- 
-  var hasWinner = false;
-  // check rows
-  for (var x = 0; x < 3; x++) {
-    var rowIsPopulated = ((model.board[x][0] !== 0) && (model.board[x][1] !== 0) && (model.board[x][2] !== 0));
-    if (model.board[x][0] === model.board[x][1] && model.board[x][1] === model.board[x][2] && rowIsPopulated) {
-      hasWinner = true;
-      colorWinningRow(x);
+var controller = {
+    playMove: function(x, y) {
+        renderMsg('');
+        // check if move is valid
+        if (model.board[x][y] !== 0) {
+          renderMsg('invalid move');
+        } else {
+          // update board array
+          model.board[x][y] = model.currentPlayer;
+          updateBoardView(x,y);
+          model.moveCounter++;
+          controller.checkGameProgress();
+          controller.toggleCurrentPlayer();
+        }
+    },
+    toggleCurrentPlayer: function() {
+        if (model.currentPlayer === 'X') {
+            model.currentPlayer = 'O';
+        } else {
+            model.currentPlayer = 'X';
+        }
+        renderCurrentPlayer();
+      },
+    checkGameProgress: function() {
+        var hasWinner = controller.checkIfWinner();
+      //  var hasWinner = false;
+        if (hasWinner) {
+          // render message to screen
+          renderMsg('WINNER: ' + model.playerName[model.currentPlayer]);
+          model.lastWinner = model.currentPlayer;
+          model.score[model.currentPlayer]++;
+          //update score board
+          updateScoreBoard(model.currentPlayer);
+        } else if (model.moveCounter === 9) {
+          // render tie message to screen
+          renderMsg('Game over: TIE');
+        }
+      },
+    checkIfWinner: function() {
+        var hasWinner = false;
+        // check rows
+        for (var x = 0; x < 3; x++) {
+          var rowIsPopulated = ((model.board[x][0] !== 0) && (model.board[x][1] !== 0) && (model.board[x][2] !== 0));
+          if (model.board[x][0] === model.board[x][1] && model.board[x][1] === model.board[x][2] && rowIsPopulated) {
+            hasWinner = true;
+            colorWinningRow(x);
+          }
+        }
+        // check columns
+        for (var x = 0; x < 3; x++) {
+          var columnIsPopulated = ((model.board[0][x] !== 0) && (model.board[1][x] !== 0) && (model.board[2][x] !== 0));
+          if (model.board[0][x] === model.board[1][x] && model.board[1][x] === model.board[2][x] && columnIsPopulated) {
+            hasWinner = true;
+            colorWinningColumn(x);
+          }
+        }
+        // check 00 to 22 diagonal
+        var firstDiagonalIsPopulated = ((model.board[0][0] !== 0) && (model.board[1][1] !== 0) && (model.board[2][2] !== 0));
+        if (model.board[0][0] === model.board[1][1] && model.board[1][1] === model.board[2][2] && firstDiagonalIsPopulated) {
+          hasWinner = true;
+          colorFirstDiagonal();
+        }
+        // check 20 to 02 diagonal
+        var secondDiagonalIsPopulated = ((model.board[2][0] !== 0) && (model.board[1][1] !== 0) && (model.board[0][2] !== 0));
+        if (model.board[2][0] === model.board[1][1] && model.board[1][1] === model.board[0][2] && secondDiagonalIsPopulated) {
+          hasWinner = true;
+          colorSecondDiagonal();
+        }
+      
+        if (hasWinner) {
+            disableButtons();
+        }
+      
+        return hasWinner;
+    },
+    saveNames: function() {
+        // get playerX
+        model.playerName.X = document.getElementById('Xname').value;
+        // get playerO
+        model.playerName.O = document.getElementById('Oname').value;
+        updatePlayerName('X');
+        updatePlayerName('O');
+        renderScoreBoardWithNames();
+        renderCurrentPlayer();
     }
-  }
-  // check columns
-  for (var x = 0; x < 3; x++) {
-    var columnIsPopulated = ((model.board[0][x] !== 0) && (model.board[1][x] !== 0) && (model.board[2][x] !== 0));
-    if (model.board[0][x] === model.board[1][x] && model.board[1][x] === model.board[2][x] && columnIsPopulated) {
-      hasWinner = true;
-      colorWinningColumn(x);
-    }
-  }
-  // check 00 to 22 diagonal
-  var firstDiagonalIsPopulated = ((model.board[0][0] !== 0) && (model.board[1][1] !== 0) && (model.board[2][2] !== 0));
-  if (model.board[0][0] === model.board[1][1] && model.board[1][1] === model.board[2][2] && firstDiagonalIsPopulated) {
-    hasWinner = true;
-    colorFirstDiagonal();
-  }
-  // check 20 to 02 diagonal
-  var secondDiagonalIsPopulated = ((model.board[2][0] !== 0) && (model.board[1][1] !== 0) && (model.board[0][2] !== 0));
-  if (model.board[2][0] === model.board[1][1] && model.board[1][1] === model.board[0][2] && secondDiagonalIsPopulated) {
-    hasWinner = true;
-    colorSecondDiagonal();
-  }
-
-  if (hasWinner) {
-      disableButtons();
-  }
-
-  return hasWinner;
 };
 
-var saveNames = function() {
-  // get playerX
-  model.playerName.X = document.getElementById('Xname').value;
-  // get playerO
-  model.playerName.O = document.getElementById('Oname').value;
-  updatePlayerName('X');
-  updatePlayerName('O');
-  renderScoreBoardWithNames();
-  renderCurrentPlayer();
-}
 
 // VIEW
 var updateBoardView = function(x, y) {
@@ -138,7 +136,7 @@ var resetBoard = function() {
     for (var y = 0; y < 3; y++) {
       var element = document.getElementById(''+ x + y);
       element.innerHTML = '';
-      element.setAttribute('onclick', 'playMove('+x+','+y+')');
+      element.setAttribute('onclick', 'controller.playMove('+x+','+y+')');
       element.style.backgroundColor = 'white';
     }
   }
