fe9d581b1bc7243f1f4e089167ae606ca9942720
Annisa <annisakaraca@gmail.com>
2018-02-13 10:46:56 -0800

    Initialize express server and implement logger middleware

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
 
+// middleware
+var myLogger = function (req, res, next) {
+    console.log(req.method + 'request receieved');
+    next();
+  } ;
+
+app.use(express.static('client'));
+app.use(myLogger);
+
+
+app.get('/', (req, res) => res.send('hello'));
+
+app.listen(3000, () => console.log('CSV report app listening on port 3000!'));
 
