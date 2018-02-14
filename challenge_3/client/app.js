3b367adcf5c03ba1858a4c92e3fe771e944842a9
Annisa <annisakaraca@gmail.com>
2018-02-14 14:02:36 -0800

    Configure react webpack babel setup and initialize server

--- challenge_3/client/app.js
+++ challenge_3/client/app.js
@@ -1,2 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const title = 'My super awesome connect four react webpack babel setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
+
+// console.log('minimal connect four react webpack babel setup');
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,6 @@
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => res.send('Hello World!'));
 
+app.listen(3000, () => console.log('connect four server listening on port 3000'));
