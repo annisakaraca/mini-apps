3596ed13129dda93adc3d1efd781d4ef55749bf8
Annisa <annisakaraca@gmail.com>
2018-02-13 17:16:25 -0800

    Indicate index of parent record on each row

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -17,13 +17,13 @@ app.use(bodyParser.json({
 
 app.post('/', (req,res) => {
   var employeeList = traverseEmployees(req.body);
+  addIndexToNodesList(employeeList);
+  addParentToNodesList(employeeList);
   var flattenedJSON = writeEmployees(employeeList);
   res.send(flattenedJSON);
 })
 
 app.listen(3000, () => console.log('CSV report app listening on port 3000!'));
-
-// var allEmployees = [];
 
 var traverseEmployees = function(node) {
   var allEmployees = [];
@@ -41,8 +41,8 @@ var traverseEmployees = function(node) {
 var writeEmployees = function(employeeList) {
   var output = getPropertiesOfList(employeeList[0]);
 
-  employeeList.forEach(function(employee, index) {
-    var employeeArray = [index];
+  employeeList.forEach(function(employee) {
+    var employeeArray = [];
     output[0].forEach(function(attribute) {
       employeeArray.push(employee[attribute]);
     })
@@ -57,4 +57,27 @@ var getPropertiesOfList = function(object) {
   var index = starterArray[0].indexOf('children');
   starterArray[0].splice(index, 1);
   return starterArray;
+};
+
+var addIndexToNodesList = function(list) {
+  var nodeCounter = 0;
+  list.forEach(function(node) {
+    node.index = nodeCounter;
+    nodeCounter++;
+  });
+}
+
+var addParentToNodesList = function(list) {
+  var addParentToChild = function(node, parentIdx) {
+    node.parent = parentIdx;
+    var children = node.children;
+    children.forEach(function(child) {
+      addParentToChild(child, node.index);
+    });
+  };
+  addParentToChild(list[0], 'n/a');
+  console.log(list);
+  // go to children of current node
+  // add parent property to each child w/ current node's idx
+  // recurse on each child
 };
