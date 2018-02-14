55293afe8dbcdd643a9a1442c844395ee92d8456
Annisa <annisakaraca@gmail.com>
2018-02-14 15:04:32 -0800

    Render basic board structure and set up react components

--- challenge_3/client/app.js
+++ challenge_3/client/app.js
@@ -1,12 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My super awesome connect four react webpack babel setup';
+const title = 'My sad incomplete connect four react webpack babel setup';
 
 ReactDOM.render(
   <div>{title}</div>,
   document.getElementById('app')
 );
+
+class Square extends React.Component {
+  render() {
+    return (
+      <td className="square">
+        <div>{this.props.value}</div>
+      </td>
+    );
+  }
+}
+
+class Row extends React.Component {
+  renderSquare(i) {
+    return <Square value={i}/>;
+  }
+
+  render() {
+    return (
+      <tr className="board-row">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+        {this.renderSquare(6)}
+    </tr>
+    );
+  }
+}
+
+class Board extends React.Component {
+
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <table>
+        <tbody>
+          <Row />
+          <Row />
+          <Row />
+          <Row />
+          <Row />
+          <Row />
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div></div>
+          <ol></ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('app')
+);
+
+
+
 
 module.hot.accept();
 
