30932b2d42c5afb0ff7e760c29890f0d7026f006
Annisa <annisakaraca@gmail.com>
2018-02-15 11:42:14 -0800

    Initialize server and webpack

--- challenge_4/client/app.js
+++ challenge_4/client/app.js
@@ -1,2 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const title = 'happy sad chocolate day';
+const title2 = 'i wont change';
 
+ReactDOM.render(
+  <div>{title}<div id='title'></div></div>,
+  document.getElementById('app')
+);
+
+ReactDOM.render(
+  <div>{title2}</div>,
+  document.getElementById('title')
+);
+
+module.hot.accept();
---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './client/app.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/client',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './client'
+  }
+}
