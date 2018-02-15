11b511747021e848228ae7a4e0c75c870e18420a
Annisa <annisakaraca@gmail.com>
2018-02-15 13:40:16 -0800

    Flesh out implementation for bowl method to update necessary values

--- challenge_4/client/game.js
+++ challenge_4/client/game.js
@@ -6,6 +6,7 @@ class Game extends React.Component {
     super(props);
     this.state = {
       score: 0,
+      currentFrame: [],
       scoresByMove: [],
       moveHistory: []
     }
@@ -13,9 +14,40 @@ class Game extends React.Component {
 
   bowl(number) {
     console.log('clicked!')
-    var newMoveHistory = this.state.moveHistory.slice();
-    newMoveHistory.push(number);
-    this.setState({moveHistory: newMoveHistory});
+    // update current frame
+    var updatedCurrentFrame = this.state.currentFrame.slice();
+    updatedCurrentFrame.push(number);
+
+    // if current frame has 2 after being updated -> update moveHistory
+    if (updatedCurrentFrame.length === 2) {
+      var newMoveHistory = this.state.moveHistory.slice();
+      newMoveHistory.push(updatedCurrentFrame);
+      this.setState({moveHistory: newMoveHistory});
+
+      // update scoresByMove depending on updatedCurrentFrame
+      var updatedScoresByMove = this.state.scoresByMove.slice();
+      if (updatedCurrentFrame[0] + updatedCurrentFrame[1] < 10){
+        updatedScoresByMove.push(updatedCurrentFrame[0] + updatedCurrentFrame[1]);
+      } else if (updatedCurrentFrame[0] + updatedCurrentFrame[1] === 10) {
+        updatedScoresByMove.push('/');
+      } else if (updatedCurrentFrame[0] === 10) {
+        updatedScoresByMove.push('X');
+      }
+      this.setState({scoresByMove: updatedScoresByMove});
+      updatedCurrentFrame = [];
+    }
+
+    // set state of currentFrame
+    this.setState({currentFrame: updatedCurrentFrame});
+
+    // var newScoresByMove = this.state.scoresByMove.slice();
+    // if (number < 10) {
+    //   newScoresByMove.push(number);
+    // } else {
+    //   newScoresByMove.push('X');
+    // }
+    // this.setState({scoresByMove: newScoresByMove});
+
   }
 
 
