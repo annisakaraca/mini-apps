54782168ad1f2e770be75146135a3f56e57bca47
Annisa <annisakaraca@gmail.com>
2018-02-14 16:13:32 -0800

    Implement drop piece button for first column

--- challenge_3/client/app.js
+++ challenge_3/client/app.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// var dropPiece = function(columnIndex) {
+//   console.log('clicked!', columnIndex);
+//   for (var row = 5; row >= 0; row--) {
+//     console.log(boardState[row][columnIndex]);
+//     if (boardState[row][columnIndex] === -1) {
+//       console.log('found!');
+//       boardState[row][columnIndex] = 1;
+//       break;
+//     }
+//   }
+//   console.dir(boardState);
+// }
+
 // state
 var boardState = [
-  [[1],[-1],[-1],[-1],[-1],[-1],[-1]],
-  [[-1],[2],[-1],[-1],[-1],[-1],[-1]],
-  [[-1],[-1],[3],[-1],[-1],[-1],[-1]],
-  [[-1],[-1],[-1],[4],[-1],[-1],[-1]],
-  [[-1],[]-1,[-1],[-1],[5],[-1],[-1]],
-  [[-1],[-1],[-1],[-1],[-1],[6],[-1]]
+  [-1, -1, -1, -1, -1, -1, -1],
+  [-1, -1, -1, -1, -1, -1, -1],
+  [-1, -1, -1, -1, -1, -1, -1],
+  [-1, -1, -1, -1, -1, -1, -1],
+  [-1, -1, -1, -1, -1, -1, -1],
+  [-1, -1, -1, -1, -1, -1, -1]
 ]
 
 class Square extends React.Component {
@@ -27,8 +40,6 @@ class Row extends React.Component {
   }
 
   render() {
-    console.dir(this.props.state);
-
     return (
       <tr className="board-row">
         {this.renderSquare(this.props.state[0])}
@@ -38,25 +49,71 @@ class Row extends React.Component {
         {this.renderSquare(this.props.state[4])}
         {this.renderSquare(this.props.state[5])}
         {this.renderSquare(this.props.state[6])}
-    </tr>
+      </tr>
+    );
+  }
+}
+
+class ButtonRow extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={() =>{
+          console.log('inbtn', this);
+          this.props.onClick(0);
+          }}>0</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+      </div>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boardState: this.props.state
+    };
+  }
+
+  dropPiece(i) {
+    console.log('inBoard', this);
+    console.log('clicked!', i);
+    var tempBoardState = this.state.boardState.slice();
+    console.dir(tempBoardState);
+    for (var row = 5; row >= 0; row--) {
+      console.log(tempBoardState[row][i]);
+      if (tempBoardState[row][i] === -1) {
+        console.log('found!');
+        tempBoardState[row][i] = 1;
+        this.setState({boardState: tempBoardState});
+        break;
+      }
+    }
+    console.dir(this.boardState);
+  }
 
   render() {
     return (
-      <table>
-        <tbody>
-          <Row state={this.props.state[0]}/>
-          <Row state={this.props.state[1]}/>
-          <Row state={this.props.state[2]}/>
-          <Row state={this.props.state[3]}/>
-          <Row state={this.props.state[4]}/>
-          <Row state={this.props.state[5]}/>
-        </tbody>
-      </table>
+      <div>
+        <ButtonRow onClick={this.dropPiece.bind(this)}/>
+        <table>
+          <tbody>
+            
+            <Row state={this.props.state[0]}/>
+            <Row state={this.props.state[1]}/>
+            <Row state={this.props.state[2]}/>
+            <Row state={this.props.state[3]}/>
+            <Row state={this.props.state[4]}/>
+            <Row state={this.props.state[5]}/>
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
