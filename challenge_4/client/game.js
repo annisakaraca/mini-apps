1ca57304918ee515a73ff8448351998e562c8613
Annisa <annisakaraca@gmail.com>
2018-02-15 14:34:57 -0800

    Calculate score for strikes and spares, with the exception of 3 strikes in a row

--- challenge_4/client/game.js
+++ challenge_4/client/game.js
@@ -28,26 +28,60 @@ class Game extends React.Component {
       var updatedScoresByMove = this.state.scoresByMove.slice();
       if (updatedCurrentFrame[0] + updatedCurrentFrame[1] < 10){
         updatedScoresByMove.push(updatedCurrentFrame[0] + updatedCurrentFrame[1]);
-      } else if (updatedCurrentFrame[0] + updatedCurrentFrame[1] === 10) {
-        updatedScoresByMove.push('/');
       } else if (updatedCurrentFrame[0] === 10) {
         updatedScoresByMove.push('X');
-      }
-      this.setState({scoresByMove: updatedScoresByMove});
+      } else if (updatedCurrentFrame[0] + updatedCurrentFrame[1] === 10) {
+        updatedScoresByMove.push('/');
+      } 
+      console.log('updated state', updatedScoresByMove);
+      this.setState({scoresByMove: updatedScoresByMove}, () => {
+        // calculate new score from scratch
+        var newScore = 0;
+        console.log('current state', this.state.scoresByMove);
+        for (var x = 0; x < this.state.scoresByMove.length; x++) {
+          if (this.state.scoresByMove[x] !== 'X' && this.state.scoresByMove[x] !== '/') {
+            newScore += this.state.scoresByMove[x];
+          } else if (this.state.scoresByMove[x] === '/') {
+            // check if updatedScoresByMove 
+            if (this.state.scoresByMove[x+1]) {
+              // replace '/' in scoresByMove
+              var scoresByMoveCopy = this.state.scoresByMove.slice();
+              var replaceSplitScore = 10 + this.state.moveHistory[x+1][0];
+              scoresByMoveCopy[x] = replaceSplitScore;
+              console.log('scoresByMoveCopy', scoresByMoveCopy);
+              this.setState({scoresByMove: scoresByMoveCopy}, () => (console.log('cb', this.state.scoresByMove)));
+              newScore += (10 + this.state.moveHistory[x + 1][0]);
+            } else {
+              newScore = 'cannot calculate yet';
+            }
+          } else if (updatedScoresByMove[x] === 'X') {
+              if (this.state.scoresByMove[x+1]) {
+                // replace '/' in scoresByMove
+                var scoresByMoveCopy = this.state.scoresByMove.slice();
+                var replaceStrikeScore = 10 + this.state.moveHistory[x+1][0] + this.state.moveHistory[x+1][1];
+                console.log('replaceStrikeScore', replaceStrikeScore);
+                scoresByMoveCopy[x] = replaceStrikeScore;
+                console.log('scoresByMoveCopy', scoresByMoveCopy);
+                this.setState({scoresByMove: scoresByMoveCopy}, () => (console.log('cb', this.state.scoresByMove)));
+                newScore += (10 + this.state.moveHistory[x + 1][0] + this.state.moveHistory[x + 1][1]);
+              } else {
+                newScore = 'cannot calculate yet';
+              }
+          }
+        }
+
+      this.setState({score: newScore});
+      });        
+
+
+
+
       updatedCurrentFrame = [];
     }
 
+
     // set state of currentFrame
     this.setState({currentFrame: updatedCurrentFrame});
-
-    // var newScoresByMove = this.state.scoresByMove.slice();
-    // if (number < 10) {
-    //   newScoresByMove.push(number);
-    // } else {
-    //   newScoresByMove.push('X');
-    // }
-    // this.setState({scoresByMove: newScoresByMove});
-
   }
 
 
