7d77fbc9e19c8fd5a799fc185cb1b5651b7f2674
Annisa <annisakaraca@gmail.com>
2018-02-15 11:46:40 -0800

    Complete app initialization

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,7 @@
+const express = require('express');
+const app = express();
 
+app.use(express.static('client'));
+app.get('/', (req, res) => res.send('hi'));
 
+app.listen(3000, () => console.log('bowling app listening on port 3000'));
